db2a2d96acc3375ed05e937a31d5916ba2e886bd
eugene-nguyen <eugene.nguyen2@gmail.com>
2025-08-01 13:10:36 -0700

    pickup rock

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,13 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class InteractableObject : MonoBehaviour
 {
+    public bool playerInRange;
     public string ItemName;
- 
+
     public string GetItemName()
     {
         return ItemName;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+{
+    if (Input.GetKeyDown(KeyCode.Mouse0) && playerInRange && SelectionManager.Instance.onTarget)
+    {
+        Debug.Log("Item added to inventory");
+        Destroy(gameObject);
+    }
+}
 }
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -6,15 +6,31 @@ using UnityEngine.UI;
  
 public class SelectionManager : MonoBehaviour
 {
- 
+    
+    public static SelectionManager Instance { get; set; }
     public GameObject interaction_Info_UI;
+    public bool onTarget;
+
     Text interaction_text;
- 
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
     private void Start()
     {
         interaction_text = interaction_Info_UI.GetComponent<Text>();
+        onTarget = false;
     }
- 
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -22,17 +38,23 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
- 
-            if (selectionTransform.GetComponent<InteractableObject>())
+
+            if (selectionTransform.GetComponent<InteractableObject>() && selectionTransform.GetComponent<InteractableObject>().playerInRange)
             {
                 interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
                 interaction_Info_UI.SetActive(true);
+                onTarget = true;
             }
-            else 
-            { 
+            else // there is a hit object, but it does not have a thing.
+            {
                 interaction_Info_UI.SetActive(false);
+                onTarget = false;
             }
- 
+
+        }
+        else // if there is no hit object.
+        {
+            interaction_Info_UI.SetActive(false);
         }
     }
 }
